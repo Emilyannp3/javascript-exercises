2f56c4e69a781a18137903ef939aa0a3f7911e8b
Emily <emily1plummer@gmail.com>
2023-01-01 15:49:08 -0600

    passed the first and second test

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,9 @@
-const repeatString = function() {
 
-};
-
+function repeatString(string, num){
+    let repeatedString = string;
+    //repeatedString.repeat(num);
+    return repeatedString.repeat(num);
+}
+repeatString("hey", 3);
 // Do not edit below this line
 module.exports = repeatString;
