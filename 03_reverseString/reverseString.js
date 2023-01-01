1d1f3677a74dc4fa829b4c6cbf80fdbe8f0cf813
Emily <emily1plummer@gmail.com>
2023-01-01 17:24:07 -0600

    completed reverse string test successfully

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,8 @@
-const reverseString = function() {
 
+function reverseString(string) {
+    const reversedString = string;
+    return reversedString.split("").reverse().join("");
 };
-
+reverseString("hello");
 // Do not edit below this line
 module.exports = reverseString;
