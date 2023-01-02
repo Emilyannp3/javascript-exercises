32b20709294f0e077a0649a9d9ccd553ef4cfc67
Emily <emily1plummer@gmail.com>
2023-01-01 18:09:02 -0600

    trying to fix my code for this test. Did not complete

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,7 @@
-const removeFromArray = function() {
-
+function removeFromArray(arr,num) {
+let removedArrayIndex = arr;
+return removedArrayIndex.slice(num);
 };
-
+removeFromArray([1, 2, 3, 4], 2);
 // Do not edit below this line
 module.exports = removeFromArray;
