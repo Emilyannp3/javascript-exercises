1b85b512331cb7a3cc169e104f72492695692407
Emilyannp3 <106265100+Emilyannp3@users.noreply.github.com>
2023-01-04 00:16:15 -0500

    added action plan for code in comments at top

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,7 +1,10 @@
-function removeFromArray(arr,num) {
-let removedArrayIndex = arr;
-return removedArrayIndex.slice(num);
+//declare an unknown array
+//take out specific index of array
+//return array without specific index
+function removeFromArray([],num) {
+let randomArray = [].slice(num);
+return randomArray;
 };
-removeFromArray([1, 2, 3, 4], 2);
+removeFromArray([1, 2, 3, 4], 3);
 // Do not edit below this line
 module.exports = removeFromArray;
