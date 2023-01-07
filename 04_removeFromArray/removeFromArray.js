617d157d6c3a99e46a6362b7d0f01d00718b6db9
Emily <emily1plummer@gmail.com>
2023-01-07 15:26:49 -0600

    finally figured out a solution to test.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,7 +1,20 @@
-function removeFromArray(arr,num) {
-let removedArrayIndex = arr;
-return removedArrayIndex.slice(num);
-};
-removeFromArray([1, 2, 3, 4], 2);
-// Do not edit below this line
-module.exports = removeFromArray;
+//declare an unknown array
+//get random argument out of array using index
+//return array without argument
+
+function removeFromArray( arr, num) {
+    let randomArray = []; 
+    for(i=0; i < arr.length ; i++){
+        if(arr[i] != num){
+            randomArray.push(arr[i]);
+            
+        }else{
+            console.log(num);
+        }
+    };
+    return randomArray;
+    };
+    removeFromArray([1, 2, 3, 4], 3);
+    // Do not edit below this line
+    module.exports = removeFromArray;
+
