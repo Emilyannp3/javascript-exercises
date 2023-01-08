bd130f82a7655a9c0035fc7cbc4bcd207ebfef59
Emily <emily1plummer@gmail.com>
2023-01-08 14:46:44 -0600

    finally figured out the answer

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -2,10 +2,14 @@
 //add the two intergers in the function
 //add the all numbers between two integers
 //maybe use a count to show numbers in between
-function sumAll(num1, num2) {
-let calculatedNumber = num1 + num2;
-return calculatedNumber;
-};
-sumAll(1, 4);
+function sumAll(num1, num2){
+    let newNum =  0;
+    for(let i = num1; i <= num2 ; i++) {
+       newNum += i;
+       console.log(newNum); 
+          
+    };
+return newNum;
+}
 // Do not edit below this line
 module.exports = sumAll;
