7083e47ad24af13d4503778f8215644142ae7503
Emily <emily1plummer@gmail.com>
2023-01-08 16:03:47 -0600

    figured out first few test, work on 700 test

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,15 @@
-const leapYears = function() {
-
-};
+function leapYears(year) {
+    
+    if (year % 4 == 0 || (year % 100 == 0 && year % 400 == 0)) {
+     ///if (year % 4 == 0) {
+    return true;   
+        
+    //return true;
+    } else {
+        return false;
+    }
+}
+leapYears();
 
 // Do not edit below this line
 module.exports = leapYears;
