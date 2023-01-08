60f031663afcfffd5baf47706517e97079d91695
Emily <emily1plummer@gmail.com>
2023-01-08 14:47:34 -0600

    completed test

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,7 +1,4 @@
-//take a function of two intergers
-//add the two intergers in the function
-//add the all numbers between two integers
-//maybe use a count to show numbers in between
+
 function sumAll(num1, num2){
     let newNum =  0;
     for(let i = num1; i <= num2 ; i++) {
