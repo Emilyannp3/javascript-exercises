4d4abcfb43e3cc4a7291cbf938c1b13ea0a7efda
Emily <emily1plummer@gmail.com>
2023-01-09 11:33:03 -0600

    completed the temp conversion test

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,14 @@
-const convertToCelsius = function() {
-};
+function convertToCelsius(degrees){
+  let newCelsius = (degrees - 32) * (5/9);
+  return newCelsius = Math.round(newCelsius * 10 ) / 10;
+}
+convertToCelsius();
 
-const convertToFahrenheit = function() {
-};
+function convertToFahrenheit(degrees){
+  let newFahrenheit = (degrees * (9/5) + 32);
+  return newFahrenheit = Math.round(newFahrenheit * 10) / 10 ;
+}
+convertToFahrenheit();
 
 // Do not edit below this line
 module.exports = {
